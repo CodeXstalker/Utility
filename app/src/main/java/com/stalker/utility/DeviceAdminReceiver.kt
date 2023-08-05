2a321a7135e285dc0abbbbf602dee1639e3612e6
Shivansh Kumar <codes.stalker@gmail.com>
2023-08-05 15:17:37 +0530

    FlightMode

---
+++ app/src/main/java/com/stalker/utility/DeviceAdminReceiver.kt
@@ -0,0 +1,11 @@
+package com.stalker.utility
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class DeviceAdminReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // No additional action required in the receiver for this example.
+    }
+}
--- app/src/main/java/com/stalker/utility/MainActivity.kt
+++ app/src/main/java/com/stalker/utility/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.stalker.utility
 
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import android.widget.Switch
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var flightMode : Button
+    lateinit var power : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewInitializer()
+
+        flightMode.setOnClickListener { operateFlightMode() }
+
+        power.setOnClickListener { operatePower() }
     }
+
+    private fun operatePower() {
+//        val devicePolicyManager = getSystemService(DEVICE_POLICY_SERVICE) as DevicePolicyManager
+//        val componentName = ComponentName(this, DeviceAdminReceiver::class.java)
+//
+//        if (devicePolicyManager.isAdminActive(componentName)) {
+//            devicePolicyManager.lockNow()
+//        } else {
+//            // The app is not a device administrator. Request for admin privileges.
+//            val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
+//            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName)
+//            intent.putExtra(
+//                DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+//                "This app requires device administrator privileges to lock the screen."
+//            )
+//            startActivity(intent)
+//        }
+    }
+
+    private fun operateFlightMode() {
+        val intent = Intent(Settings.ACTION_AIRPLANE_MODE_SETTINGS)
+        startActivity(intent)
+    }
+
+    private fun viewInitializer() {
+
+        flightMode = findViewById(R.id.FlightMode)
+        power = findViewById(R.id.Power)
+
+    }
+
 }
